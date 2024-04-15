664e3de373c1e657625bb714b5cd0737e80e64f2
Pieter Visser <50768004+wasdafor@users.noreply.github.com>
2024-04-15 22:29:33 +0200

    Added a bit more randomization to the serial port output to make it feal more realistic.

--- lib/Helpers/serialMocker.dart
+++ lib/Helpers/serialMocker.dart
@@ -6,16 +6,24 @@ extension SerialMocker on SerialPort {
   void listen(Function(Uint8List data) event) async {
     const int maxHeartRate = 190;
     const int minHeartRate = 70;
-    const int strength = 4;
+    const int minSegmentLength = 6;
+    const int maxSegmentLength = 12;
+    const int minDelay = 500;
+    const int maxDelay = 1000;
+    const int strength = 2;
+    const int randomStrengthMultiplyer = 5;
     final Random random = Random();
 
     int heartBeat = 120;
     int state = 1;
 
     while (true) {
-      for (int i = 0; i < 10; i++) {
+      int segmentLength = random.nextInt(maxSegmentLength - minSegmentLength) +
+          minSegmentLength;
+      for (int i = 0; i < segmentLength; i++) {
         // Adding possible change
-        heartBeat = (heartBeat + (random.nextBool() ? (state * strength) : 0));
+        int newStrenth = strength * random.nextInt(randomStrengthMultiplyer);
+        heartBeat = (heartBeat + (state * newStrenth));
 
         // Claming the hearbeat to some proper values
         heartBeat = heartBeat.clamp(minHeartRate, maxHeartRate);
@@ -24,7 +32,8 @@ extension SerialMocker on SerialPort {
         state = random.nextInt(3) - 1;
 
         // Waiting for some random time
-        await Future.delayed(Duration(milliseconds: random.nextInt(500) + 500));
+        await Future.delayed(Duration(
+            milliseconds: random.nextInt(maxDelay - minDelay) + minDelay));
 
         // Sending the date to mock the serial port
         event(Uint8List.fromList([heartBeat]));
