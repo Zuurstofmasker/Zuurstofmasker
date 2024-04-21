629978f8f5d53ff10217c07113431855f7c5dc68
SanCWvS <57943836+SanCWvS@users.noreply.github.com>
2024-04-21 19:55:30 +0200

    Commit before merge

--- lib/main.dart
+++ lib/main.dart
@@ -384,3 +384,96 @@ class Button extends StatelessWidget {
     );
   }
 }
+
+class PatientFormPage extends StatelessWidget {
+  const PatientFormPage ({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Form sample')),
+        body: const PatientForm(),
+      ),
+    );
+  }
+}
+
+class PatientForm extends StatelessWidget {
+  const PatientForm({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: '0',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Invalid input!';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: '0',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Invalid input!';
+              }
+              return null;
+            },
+          ),TextFormField(
+            decoration: const InputDecoration(
+              hintText: '0',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Invalid input!';
+              }
+              return null;
+            },
+          ),TextFormField(
+            decoration: const InputDecoration(
+              hintText: '0',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Invalid input!';
+              }
+              return null;
+            },
+          ),TextFormField(
+            decoration: const InputDecoration(
+              hintText: '0',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Invalid input!';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
